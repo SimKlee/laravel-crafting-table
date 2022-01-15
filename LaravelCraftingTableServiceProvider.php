afb046c09f01e8ecf013fc67aff6c53081602512
Simon Kleeschulte <simon.kleeschulte@dr-walter.com>
2022-01-15 10:05:48 +0100

    added ServiceProvider for package

---
+++ LaravelCraftingTableServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimKlee\LaravelCraftingTable;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCraftingTableServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
